64bfc62dd540b5a881fdda51301893967d308208
Lukas Matulis <lukas.matulis@vvk.lt>
2026-03-25 14:58:47 +0200

    Added Role and Conference models with relationships

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Conference extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'date',
+        'location',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
